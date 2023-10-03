9cc7157690b0241f8a72eef6ba44fb2084ffe152
Victor Martins <martinsvictor287@gmail.com>
2023-10-03 23:22:36 +0300

    update imports for containers

--- src/containers/index.tsx
+++ src/containers/index.tsx
@@ -1,24 +1,25 @@
-import CreateHighlight from './Promotions/CreateHighlight';
-import CreatePromotions from './Promotions/CreatePromotions';
-import Customer from './Customers/Customer/Customer';
-import Customers from './Customers/Customers';
-import Dashboard from './Dashboard/Dashboard';
-import EditMenu from './Restaurants/Restaurant/EditMenu';
-import Menu from './Menu';
-import MenuCards from './Restaurants/Restaurant/MenuCards';
-import Notifications from './Notifications';
-import Order from './Order/Order';
-import Promotions from './Promotions/Promotions';
-import Restaurant from './Restaurants/Restaurant/Restaurant';
-import Restaurants from './Restaurants/Restaurants';
-import Revenue from './Revenue/Revenue';
-import Reviews from './Reviews/Reviews';
-import RiderDetail from './Riders/RiderDetail';
-import Riders from './Riders/Riders';
-import Settings from './Settings';
-import Users from './Users/Users';
-import ViewHighlight from './Promotions/ViewHighlight';
-import ViewPromotions from './Promotions/ViewPromotions';
+import { CreateHighlight } from '../components';
+import { CreatePromotions } from '../components';
+import { Customer } from '../components';
+import { Customers } from '../components';
+import { Dashboard } from '../components';
+import { EditMenu } from '../components';
+import { Menu } from '../components';
+import { MenuCards } from '../components';
+import { Notifications } from '../components';
+import { Order } from '../components';
+import { Promotions } from '../components';
+import { Restaurant } from '../components';
+import { Restaurants } from '../components';
+import { Revenue } from '../components';
+import { Reviews } from '../components';
+import { RiderDetail } from '../components';
+import { Riders } from '../components';
+import { Settings } from '../components';
+import { Users } from '../components';
+import { ViewHighlight } from '../components';
+import { ViewPromotions } from '../components';
+
 export {
 	Dashboard,
 	Restaurants,
